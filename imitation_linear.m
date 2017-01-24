02a63f5ea2441268c866b567f8e35b1d9f5ea7cb
SrikanthMalla <malla.srikanth2012@vit.ac.in>
2017-01-24 00:41:29 -0500

    trajectories generated with random gains and polynomial fit is done

--- imitation_linear.m
+++ imitation_linear.m
@@ -17,20 +17,24 @@ end
 xo = [50; 0];% initial condition
 k=[0,0];
 
-x1=[]; %trajectories list for first variable
-x2=[]; %trajectories list for second variable
-T=[];
-
+x1=[];x2=[]; %trajectories list
+ti=[]; %times list
 %generate different trajectories for the simulation 
 for i=0.1:0.1:1
-    [t,x]=ode45(@(t,x)linear_ode(A,B,C,k+i,t,x),[0,20],xo)
-%     plot(t,x);
-%     ti=[ti;t];
-%     T=[T;x';]
-    pause(1);
+    [t,x]=ode45(@(t,x)linear_ode(A,B,C,k+i,t,x),[0:0.01:20],xo);
+    ti=t';
+    x1=[x1;x(:,1)'];
+    x2=[x2;x(:,2)'];
 end
-
+x1=mean(x1);
+x2=mean(x2);
+ord=10;
 %fit polynomial curve to the data
-p=polyfit(ti,T,n)
+figure(1)
+p1=polyfit(ti,x1,ord);
+plot(ti,polyval(p1,ti));
+figure(2);
+p2=polyfit(ti,x2,ord);
+plot(ti,polyval(p2,ti));
 
 
